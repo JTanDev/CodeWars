c902aa509d2302560e77f90c9c830e6205f95c9b
Jeremy Tanner <jtannerdev@gmail.com>
2022-05-10 17:29:42 -0400

    added yourTestAvg

---
+++ 8kyu/yourTestVsAvg.js
@@ -0,0 +1,10 @@
+// There was a test in your class and you passed it. Congratulations!
+// But you're an ambitious person. You want to know if you're better than the average student in your class.
+
+// You receive an array with your peers' test scores. Now calculate the average and compare your score!
+
+// Return True if you're better, else False!
+
+function betterThanAverage(classP, yourP) {
+  return classP.reduce((p, c) => p + c) / classP.length <= yourP;
+}
