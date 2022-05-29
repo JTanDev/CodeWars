302adbf085157d064b4f6c2bcc7f0b481a83e48e
Jeremy Tanner <jtannerdev@gmail.com>
2022-05-29 11:11:38 -0400

    narcNumber

---
+++ 7kyu/narcNumber.js
@@ -0,0 +1,12 @@
+// A Narcissistic Number is a number of length l,
+// in which the sum of its digits to the power of l is equal to the original number.
+// If this seems confusing, refer to the example below.
+// Ex: 153, where l = 3 ( the number of digits in 153 )
+// 1**3 + 5**3 + 3**3 = 153
+// Write a function that, given n, returns whether or not n is a Narcissistic Number.
+
+function isNarcissistic(n) {
+  let arr = n.toString().split("");
+  let num = arr.reduce((acc, cur) => (acc += Number(cur) ** arr.length), 0);
+  return num == n;
+}
